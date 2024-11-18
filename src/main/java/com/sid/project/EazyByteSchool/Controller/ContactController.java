ecccd8385dfa8fdcd970b60d407efd5b022f5908
Siddhant Prakash Kadam <86238624+sidhantkadam@users.noreply.github.com>
2024-11-18 23:06:54 +0530

    Updated contact page with docker and buildspec.yaml file

--- src/main/java/com/sid/project/EazyByteSchool/Controller/ContactController.java
+++ src/main/java/com/sid/project/EazyByteSchool/Controller/ContactController.java
@@ -2,13 +2,16 @@ package com.sid.project.EazyByteSchool.Controller;
 
 import com.sid.project.EazyByteSchool.ContactService;
 import com.sid.project.EazyByteSchool.Model.ContactInfo;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,16 +26,22 @@ public class ContactController
         this.contactService = contactService;
     }
 
-    @GetMapping("/contact")
-    public String displayContactUs()
+    @RequestMapping("/contact")
+    public String displayContactUs(Model model)
     {
+        model.addAttribute("contact", new ContactInfo());
         return "contact.html";
     }
 
     @PostMapping("/saveMsg")
-    public ModelAndView createContact(ContactInfo contactInfo)
+    public String createContact(@Valid @ModelAttribute("contact") ContactInfo contactInfo, Errors errors)
     {
+        if(errors.hasErrors())
+        {
+            log.error("Contact form validation failed due to : "+ errors.toString());
+            return "contact.html";
+        }
         contactService.doContact(contactInfo);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
--- src/main/java/com/sid/project/EazyByteSchool/Model/ContactInfo.java
+++ src/main/java/com/sid/project/EazyByteSchool/Model/ContactInfo.java
@@ -1,17 +1,35 @@
 package com.sid.project.EazyByteSchool.Model;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class ContactInfo
 {
-    private final String name;
-    private final String message;
-    private final String mobileNum;
-    private final String email;
-    private final String subject;
+    @NotBlank(message="Name must not be blank")
+    @Size(min=3, message="Name must be at least 3 characters long")
+    private String name;
+
+    @NotBlank(message="Mobile number must not be blank")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+    private String mobileNum;
+
+    @NotBlank(message="Email must not be blank")
+    @Email(message = "Please provide a valid email address" )
+    private String email;
+
+    @NotBlank(message="Subject must not be blank")
+    @Size(min=5, message="Subject must be at least 5 characters long")
+    private String subject;
+
+    @NotBlank(message="Message must not be blank")
+    @Size(min=10, message="Message must be at least 10 characters long")
+    private String message;
 
     @Override
     public String toString()
